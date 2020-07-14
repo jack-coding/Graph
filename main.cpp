09356e7296e5a964e549ef711be37f2113e05e59
jack-coding <2515906731@qq.com>
2020-07-14 23:06:53 +0800

    1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<Windows.h>
+#include"Graph.h"
+
+using namespace std;
+
+int main(int argc, char * argv) {
+	Graph g1;
+	g1.Create();
+	g1.showBFS();
+	system("pause");
+	return EXIT_SUCCESS;
+}
